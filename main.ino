d3ae74bc99b27a6d85e71857dbcd816f14a8a52c
MengerianOrthodox <103938034+MengerianOrthodox@users.noreply.github.com>
2022-08-30 16:13:08 -0300

    Agregue clase del motor

--- main.ino
+++ main.ino
@@ -1,33 +1,80 @@
-class Pulsador {
-  private:
-    int pin;
-    bool flanco = HIGH;
-    bool estado_anterior = !flanco;
+class Motor
+{
+private:
+    
+    int default_speed = 100;
+    int pin_dir, pin_pwm, speed;
+    bool invert;
 
-    //metodo
-  public:
-    Pulsador(int p) {
-      pin = p;
-
-      pinMode(pin, INPUT);
+public:
+    Motor(int pdir, int ppwm)
+    {
+        pin_dir = pdir;
+        pin_pwm = ppwm;
+        pinMode(pin_dir, OUTPUT);
+        pinMode(pin_pwm, OUTPUT);
+        
+        speed = default_speed;
     }
 
-    void setFlanco(bool f) {
-      flanco = f;
-      estado_anterior = !flanco;
+    void setSpeed(int s)
+    {
+        if (s >= 100)
+        {
+            s = 100;
+        }
+        else if (s <= 0)
+        {
+            s = 0;
+        }
+
+        int value = (int)s * 255 / 100;
+        speed = value;
     }
 
-    //metodos o acciones
-    bool getIsPress() {
-      bool estado_actual = digitalRead(pin);
-      bool estado = (estado_anterior != estado_actual) && estado_actual == flanco;
-      estado_anterior = estado_actual;
-      return estado;
+    void forward()
+    {
+
+        digitalWrite(pin_dir, invert);
+        analogWrite(pin_pwm, speed);
     }
 
-    String getIsPressText() {
-      if (getIsPress()) return "PRESS";
-      else return "IS NOT PRESS";
+    void backward() 
+    {
+        digitalWrite(pin_dir, !invert);
+    
+        analogWrite(pin_pwm, speed);
     }
 
+    void invertDirection()
+    {
+        
+        invert = !invert;
+    }
+
+    void stop() 
+    {
+        digitalWrite(pin_dir, LOW);
+        analogWrite(pin_pwm, 0);
+    }
 };
+
+#define PIN_MOTOR_DERECHO_DIR 4
+#define PIN_MOTOR_DERECHO_PWM 5
+#define PIN_MOTOR_IZQUIERDO_DIR 6
+#define PIN_MOTOR_IZQUIERDO_PWM 7
+
+Motor m1 = Motor(PIN_MOTOR_DERECHO_DIR, PIN_MOTOR_DERECHO_PWM);
+Motor m2 = Motor(PIN_MOTOR_IZQUIERDO_DIR, PIN_MOTOR_IZQUIERDO_PWM);
+
+void forward() 
+{
+    m1.forward();
+    m2.forward();
+}
+
+void backward() 
+{
+    m1.backward();
+    m2.backward();
+}
